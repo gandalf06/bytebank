b5be2a8358ebdc52e74e4c3438fb9de507a028c9
Darlan Oliveira <feitt.c6@gmail.com>
2021-02-28 15:48:09 -0300

    entendendo sistema de widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Text('Welcome to ByteBank',textDirection: TextDirection.ltr));
-
+void main() => runApp(
+      Column(
+        children: <Widget>[
+          Text('Deliver features faster', textDirection: TextDirection.ltr),
+          Text('Craft beautiful UIs', textDirection: TextDirection.ltr),
+          Text('Craft beautiful UIs', textDirection: TextDirection.ltr),
+          Column(
+            children: <Widget> [
+              Text(
+                'Welcome to ByteBank',
+                textDirection: TextDirection.ltr,
+              )
+            ],
+          )
+        ],
+      ),
+    );
